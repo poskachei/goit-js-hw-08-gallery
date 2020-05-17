8af4c5b6981a08493e5116d43c0794a1967582fe
poskachei <vposkachey@gmail.com>
2020-05-17 19:12:42 +0300

    fixed2

--- js/gallery.js
+++ js/gallery.js
@@ -4,13 +4,15 @@ import { default as imagesCollection } from "./gallery-items.js";
 
 const refGallery = document.querySelector(".js-gallery");
 const refLightBox = document.querySelector(".js-lightbox");
-const refPopUpImg = document.querySelector(".lightbox___image");
-const refCloseBtn = document.querySelector("button[data-action=close-modal]");
+const refPopUpImg = document.querySelector(".lightbox__image");
+const refCloseBtn = document.querySelector(
+  'button[data-action="close-lightbox"]'
+);
 
 const imagesToPage = imagesCollection.reduce((acc, img) => {
   img = `<li class="gallery__item">
             <a class="gallery__link" href="${img.original}">
-               <img class="gallery__image" src="${img.preview}" data-source="${img.original}" alt="${img.description}"/>
+               <img class="gallery__image" src="${img.original}" data-source="${img.preview}" alt="${img.description}"/>
             </a>
         </li>`;
   return (acc += img);
@@ -19,10 +21,10 @@ const imagesToPage = imagesCollection.reduce((acc, img) => {
 refGallery.insertAdjacentHTML("afterbegin", imagesToPage);
 
 const openPopUp = (e) => {
+  e.preventDefault();
   if (e.target === e.currentTarget) {
     return;
   }
-  e.preventDefault();
   refLightBox.classList.add("is-open");
   refPopUpImg.setAttribute("src", `${e.target.getAttribute("src")}`);
 };
